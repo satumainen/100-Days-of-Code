3c275feae4bea8d9d33c68583a88ad3145092069
satumainen <satulaukka@gmail.com>
2025-01-15 14:50:34 +0200

    Add Day 16

--- Day 16 - Coffee Machine OOP/main.py
+++ Day 16 - Coffee Machine OOP/main.py
@@ -1,4 +1,23 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
-from money_machine import Mo
+from money_machine import MoneyMachine
 
+def main():
+    menu = Menu()
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+    is_machine_on = True
+    while is_machine_on:
+        choice = input(f"What would you like? {menu.get_items()}: ")
+        if choice == "report":
+            coffee_maker.report()
+            money_machine.report()
+        elif choice == "off":
+            is_machine_on = False
+        else:
+            menu_item = menu.find_drink(choice)
+            if coffee_maker.is_resource_sufficient(menu.find_drink(menu_item.name)) and money_machine.make_payment(menu_item.cost):
+                coffee_maker.make_coffee(menu_item)
+
+
+main()
