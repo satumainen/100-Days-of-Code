5469502e0e50cd889ca104926057671bdea17e0c
satumainen <satulaukka@gmail.com>
2025-04-02 14:59:23 +0300

    Add Completed Day 39

--- Day 39 - Capstone Part 1 - Fligth Deal Finder/flight_search.py
+++ Day 39 - Capstone Part 1 - Fligth Deal Finder/flight_search.py
@@ -1,6 +1,15 @@
 import os
 import requests
 
+#Remember to hide
+AMADEUS_FLIGHT_SEARCH_SECRET = os.environ['AMADEUS_FLIGHT_SEARCH_SECRET']
+AMADEUS_FLIGHT_SEARCH_API_KEY = os.environ['AMADEUS_FLIGHT_SEARCH_API_KEY']
+
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+
+
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     ##documentation: https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api-reference
@@ -9,12 +18,76 @@ class FlightSearch:
     #https://developers.amadeus.com/
 
 
-    AMADEUS_FLIGHT_SEARCH_API = ""
 
     def __init__(self):
+        self._token = self._get_new_token()
         self.flights = []
 
-    def get_iata_code(self, city):
-        print("In Flight Search")
-        code = "test"
+    def get_iata_code(self, city_name):
+        print(f"Using this token to get destination {self._token}")
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "keyword": city_name,
+            "max": "2",
+            "include": "AIRPORTS",
+        }
+        response = requests.get(
+            url=IATA_ENDPOINT,
+            headers=headers,
+            params=query
+        )
+
+        print(f"Status code {response.status_code}. Airport IATA: {response.text}")
+        try:
+            code = response.json()["data"][0]['iataCode']
+        except IndexError:
+            print(f"IndexError: No airport code found for {city_name}.")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport code found for {city_name}.")
+            return "Not Found"
         return code
+
+
+    def _get_new_token(self):
+        header = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        body = {
+            'grant_type': 'client_credentials',
+            'client_id': AMADEUS_FLIGHT_SEARCH_API_KEY,
+            'client_secret': AMADEUS_FLIGHT_SEARCH_SECRET
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+
+        # New bearer token. Typically expires in 1799 seconds (30min)
+        print(f"Your token is {response.json()['access_token']}")
+        print(f"Your token expires in {response.json()['expires_in']} seconds")
+        return response.json()['access_token']
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "GBP",
+            "max": "10",
+        }
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+        return response.json()
--- Day 39 - Capstone Part 1 - Fligth Deal Finder/notification_manager.py
+++ Day 39 - Capstone Part 1 - Fligth Deal Finder/notification_manager.py
@@ -1,6 +1,23 @@
-import requests
+import os
+from twilio.rest import Client
 
+# Using a .env file to retrieve the phone numbers and tokens.
+
+MY_TWILIO_NR = os.environ['MY_TWILIO_NR']
+MY_NR = os.environ['MY_NR']
+TWILIO_SID = os.environ.get("ACC_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
 
 class NotificationManager:
 
-    pass
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message_body):
+        message = self.client.messages.create(
+            from_=MY_TWILIO_NR,
+            body=message_body,
+            to=MY_NR
+        )
+        # Prints if successfully sent.
+        print(message.sid)
